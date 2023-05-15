4f2182ac4ca81ad211534034773fc6d224d664b3
giomenezes <giovanna.rodrigues@sptech.school>
2023-05-15 10:39:42 -0300

    style(header): Adicionando hover ao style e integrando cadastro&login ao mesmo

--- code/assets/components/header.js
+++ code/assets/components/header.js
@@ -1,6 +1,6 @@
 const header = document.createElement('template');
 
-header.innerHTML = 
+header.innerHTML =
     `<header>
         <a href="../home/home.html"><img class="logotipo" src="../assets/logoWBg.png" alt="Logo: Inside"></a>
         <div id="divListas">
@@ -8,7 +8,13 @@ header.innerHTML =
                 <li class="liZoom"><a href="../estilos/estilos.html">Estilos</a></li>
                 <li class="liZoom"><a href="../posts/posts.html">Posts</a></li>
                 <li class="liZoom"><a href="../quiz/quiz.html">Quiz</a></li>
-                <a href="../login/login.html"><img src="../assets/user.png" alt="Icon: User"></a>
+                <div class="dropdown">
+                        <img src="../assets/user.png" alt="Icon: User" id="user-icon" class="dropbtn">
+                        <div class="dropdown-content">
+                            <a href="../cadastro&login/cadastro.html">Cadastro</a>
+                            <a href="../cadastro&login/login.html">Login</a>
+                        </div>
+                </div>
             </ul>
         </div>
     </header>`
