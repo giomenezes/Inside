bd58a58ca195de6e9833241a59bcb4e81994a2d9
giomenezes <giovanna.rodrigues@sptech.school>
2023-05-21 02:33:22 -0300

    feat(estilos): Adicionando mais estilos

--- code/assets/components/header.js
+++ code/assets/components/header.js
@@ -2,12 +2,12 @@ const header = document.createElement('template');
 
 header.innerHTML =
     `<header>
-        <a href="../index/index.html"><img class="logotipo" src="../assets/logoWBg.png" alt="Logo: Inside"></a>
+        <a href="../home/home.html"><img class="logotipo" src="../assets/logoWBg.png" alt="Logo: Inside"></a>
         <div id="divListas">
             <ul>
                 <li class="liZoom"><a href="../estilos/estilos.html">Estilos</a></li>
                 <li class="liZoom"><a href="../posts/posts.html">Posts</a></li>
-                <li class="liZoom"><a href="../quiz/beginning.html">Quiz</a></li>
+                <li class="liZoom"><a href="../quiz/start.html">Quiz</a></li>
                 <div class="dropdown">
                         <img src="../assets/user.png" alt="Icon: User" id="user-icon" class="dropbtn">
                         <div class="dropdown-content">
